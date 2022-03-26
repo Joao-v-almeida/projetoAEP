36bd732dea693421914a976109e7d6a6ba471c62
Joao-v-almeida <jaovittor.almeida@gmail.com>
2022-03-26 14:14:57 -0300

    Commit inicial, ambiente do projeto. Criado: DAO (Conexão, DAOUusario) | MODEL (Usuario). Database, 3 páginas JSP (index, login, cadastro)

---
+++ projetoAEP/src/java/MODEL/Usuario.java
@@ -0,0 +1,65 @@
+package MODEL;
+
+public class Usuario {
+
+    private int idUsuario;
+    private String nomeUsuario;
+    private String emailUsuario;
+    private String telefoneUsuario;
+    private String dataNascimentoUsuario;
+    private String senhaUsuario;
+    private String dataCadastroUsuario;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getTelefoneUsuario() {
+        return telefoneUsuario;
+    }
+
+    public void setTelefoneUsuario(String telefoneUsuario) {
+        this.telefoneUsuario = telefoneUsuario;
+    }
+
+    public String getDataNascimentoUsuario() {
+        return dataNascimentoUsuario;
+    }
+
+    public void setDataNascimentoUsuario(String dataNascimentoUsuario) {
+        this.dataNascimentoUsuario = dataNascimentoUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public String getDataCadastroUsuario() {
+        return dataCadastroUsuario;
+    }
+
+}
